ac90bfd8e350afade44ee77039c295fadac449ac
BnC <BnC@DESKTOP-N9R3CAB.simmtech.com>
2023-05-11 16:17:06 +0900

    - Generate Question, Answer
    - Packaging

--- SpringBootBoard_PersonalEdition/src/test/java/com/springBootBoard_PersonalEdition/SpringBootBoardPersonalEditionApplicationTests.java
+++ SpringBootBoard_PersonalEdition/src/test/java/com/springBootBoard_PersonalEdition/SpringBootBoardPersonalEditionApplicationTests.java
@@ -1,13 +1,150 @@
 package com.springBootBoard_PersonalEdition;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.springBootBoard_PersonalEdition.Answer.Answer;
+import com.springBootBoard_PersonalEdition.Question.Question;
+import com.springBootBoard_PersonalEdition.Question.QuestionRepository;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 class SpringBootBoardPersonalEditionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+//	@Autowired
+//	private AnswerRepository answerRepository;
+	
+//	@Test
+//	void testJpa() {
+//		Question q1 = new Question();
+//		q1.setSubject("Test Subject 01");
+//		q1.setContent("Test Content 01");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1);
+//		
+//		Question q2 = new Question();
+//		q2.setSubject("Test subject 02");
+//		q2.setContent("Test content 02");
+//		q2.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q2);
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		List<Question> all = this.questionRepository.findAll();
+//		assertEquals(2, all.size());
+//		
+//		Question q = all.get(0);
+//		assertEquals("Test Subject 01", q.getSubject());
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		
+//		if(oq.isPresent()) {
+//			Question q = oq.get();
+//			assertEquals("Test Subject 01",q.getSubject());
+//		}
+//	}
 
+//	@Test
+//	void testJpa() {
+//		List<Question> question = this.questionRepository.findBySubjectAndContent("Test Subject 01", "Test Content 01");
+//		assertEquals(1, question.get(0).getId());
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		List<Question> question = this.questionRepository.findBySubjectLike("%Test Subject 01%");
+//		assertEquals(1, question.get(0).getId());
+//		assertEquals("Test Content 01", question.get(0).getContent());
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		
+//		Question question = oq.get();
+//		assertEquals("Test content 02", question.getContent());
+//		
+//		question.setSubject("Test Subject #02");
+//		question.setContent("Test Content #02");
+//		this.questionRepository.save(question);
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		List<Question> question = this.questionRepository.findBySubject("Test Subject #01");
+//		
+//		assertEquals("Test Content #01", question.get(0).getContent());
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		Question q1 = new Question();
+//		q1.setSubject("Test Subject #03");
+//		q1.setContent("Test Content #03");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1);
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		assertEquals(3,this.questionRepository.count());
+//		
+//		Optional<Question> oq = this.questionRepository.findById(3);
+//		assertTrue(oq.isPresent());
+//		Question question = oq.get();
+//		this.questionRepository.delete(question);
+//		
+//		oq = this.questionRepository.findById(3);
+//		assertTrue(oq.isEmpty());
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question question = oq.get();
+//		
+//		Answer answer = new Answer();
+//		answer.setQuestion(question);
+//		answer.setContent("Test Answer #01");
+//		answer.setCreateDate(LocalDateTime.now());
+//		this.answerRepository.save(answer);
+//	}
+	
+//	@Test
+//	void testJpa() {
+//		Optional<Answer> oa = this.answerRepository.findById(1);
+//		assertTrue(oa.isPresent());
+//		Answer answer = oa.get();
+//		assertEquals("Test Answer #01", answer.getContent());
+//	}
+	
+	@Test
+	@Transactional
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question question = oq.get();
+		
+		List<Answer> answerList = question.getAnswerList();
+		
+		assertEquals(1, answerList.size());
+		assertEquals("Test Answer #01", answerList.get(0).getContent());
+	}
 }
