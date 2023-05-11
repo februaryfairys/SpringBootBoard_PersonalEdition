3d6ed020d0f9bb10b5331405fa7171cea8e0dec1
BnC <BnC@DESKTOP-N9R3CAB.simmtech.com>
2023-05-11 17:41:29 +0900

    - add Thymeleaf Template

---
+++ SpringBootBoard_PersonalEdition/src/main/java/com/springBootBoard_PersonalEdition/Question/QuestionController.java
@@ -0,0 +1,25 @@
+package com.springBootBoard_PersonalEdition.Question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/question")
+@RequiredArgsConstructor
+public class QuestionController {
+
+	private final QuestionRepository questionRepository;
+	@GetMapping("/list")
+	public String list(Model model) {
+		
+		List<Question> questionList = this.questionRepository.findAll();
+		model.addAttribute("questionList", questionList);
+		return "question_list";
+	}
+}
