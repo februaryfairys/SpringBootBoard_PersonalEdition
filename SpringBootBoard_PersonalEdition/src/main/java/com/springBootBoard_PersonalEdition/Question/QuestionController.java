1b26ea1bfcab95a520b8f00e5ea75c75eefebf5d
BnC <BnC@DESKTOP-N9R3CAB.simmtech.com>
2023-06-01 17:21:24 +0900

    - Generate Question Detail - Generate DataNotFoundException Class

--- SpringBootBoard_PersonalEdition/src/main/java/com/springBootBoard_PersonalEdition/Question/QuestionController.java
+++ SpringBootBoard_PersonalEdition/src/main/java/com/springBootBoard_PersonalEdition/Question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -22,5 +23,12 @@ public class QuestionController {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
+	}
+	
+	@GetMapping("/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 }
