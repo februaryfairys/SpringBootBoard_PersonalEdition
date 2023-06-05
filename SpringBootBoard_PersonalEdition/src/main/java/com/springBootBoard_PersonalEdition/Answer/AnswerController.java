338b9dbe62377996eaa2636a6acf9268901dc010
BnC <BnC@DESKTOP-N9R3CAB.simmtech.com>
2023-06-05 17:00:59 +0900

    - Generate Answer in View

---
+++ SpringBootBoard_PersonalEdition/src/main/java/com/springBootBoard_PersonalEdition/Answer/AnswerController.java
@@ -0,0 +1,30 @@
+package com.springBootBoard_PersonalEdition.Answer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.springBootBoard_PersonalEdition.Question.Question;
+import com.springBootBoard_PersonalEdition.Question.QuestionService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+
+	private final QuestionService questionService;
+	private final AnswerService   answerService;
+	
+	@PostMapping("/create/{id}")
+	public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+		
+		Question question = this.questionService.getQuestion(id);
+		this.answerService.create(question, content);
+		return String.format("redirect:/question/detail/%s", id);
+	}
+}
