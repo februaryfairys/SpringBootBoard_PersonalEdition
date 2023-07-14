640ebe24f7a64465cde36dc4117a270f15529f27
user <user@DESKTOP-FCNK2NQ>
2023-07-15 01:20:03 +0900

    - Update answer, question package names.

--- SpringBootBoard_PersonalEdition/src/main/java/com/springBootBoard_PersonalEdition/Answer/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springBootBoard_PersonalEdition.Answer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer>{
-
-}
